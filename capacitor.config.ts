3cbe0206fcbdf34550bad9f0e09a51a74d9f24a0
rbauer2024 <168412926+rbauer2024@users.noreply.github.com>
2025-11-19 16:26:06 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// @ts-nocheck
+
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.siku.kalkulator',
   appName: 'SIKU Heizplatten Empfehlungskalkulator',
-  webDir: 'build'
+  webDir: 'build',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      backgroundColor: "#FFFFFF"
+    }
+  },
+  server: {
+    url: 'https://siku-kalkulator.vercel.app/',
+    cleartext: true
+  }
 };
 
 export default config;
