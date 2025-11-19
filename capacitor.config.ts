f2278a279c5c43c42e2e82eb117e23b1cfa7a6be
rbauer2024 <168412926+rbauer2024@users.noreply.github.com>
2025-11-19 16:20:36 +0100

    Update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.siku.kalkulator',
+  appName: 'SIKU Heizplatten Empfehlungskalkulator',
+  webDir: 'build'
+};
+
+export default config;
